8fde45224bc1e15de5637db27015cede2ddcee7e
SHUBHAM DWIVEDI <121412294+dwivedishubham545@users.noreply.github.com>
2024-02-19 19:01:35 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Overlapping Intervals/overlapping-intervals.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+public:
+    vector<vector<int>> overlappedInterval(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end());
+        vector<vector<int>> ans = {intervals[0]};
+        for(int i = 1; i < intervals.size(); i++){
+            if(ans.back()[1] >= intervals[i][1]) continue;
+            else if(ans.back()[1] >= intervals[i][0] && ans.back()[1] < intervals[i][1]) ans.back()[1] = intervals[i][1];
+            else if(ans.back()[1] < intervals[i][0]) ans.push_back(intervals[i]);
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n;
+		cin >> n;
+		vector<vector<int>>Intervals(n);
+		for(int i = 0; i < n; i++){
+			int x, y;
+			cin >> x >> y;
+			Intervals[i].push_back(x);
+			Intervals[i].push_back(y);
+		}
+		Solution obj;
+		vector<vector<int>> ans = obj.overlappedInterval(Intervals);
+		for(auto i: ans){
+			for(auto j: i){
+				cout << j << " ";
+			}
+		}
+		cout << "\n";
+	}
+	return 0;
+}
+// } Driver Code Ends
