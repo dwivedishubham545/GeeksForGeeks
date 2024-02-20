80a49deb8e8004aaa58631b5490e04869ca200d6
SHUBHAM DWIVEDI <121412294+dwivedishubham545@users.noreply.github.com>
2024-02-20 07:49:18 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Word Break/word-break.cpp
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution
+{
+public:
+    int solve(int i, string &s, set<string>& st,vector<int>& dp){
+        if(i == s.length()) return 1;
+        if(dp[i] != -1) return dp[i];
+        string temp = "";
+        for(int j=i;j<s.length();j++){
+            temp += s[j];
+            if(st.find(temp) != st.end()){
+                if(solve(j+1,s,st,dp)) return dp[i] = 1;
+            }
+        }
+        return dp[i] = 0;
+    }
+    int wordBreak(string A, vector<string> &B) {
+        vector<int> dp(A.length()+1, -1);
+        set<string> st;
+        for(auto i:B){
+            st.insert(i);
+        }
+        return solve(0,A,st,dp);
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<string> dict;
+        for(int i=0;i<n;i++){
+            string S;
+            cin>>S;
+            dict.push_back(S);
+        }
+        string line;
+        cin>>line;
+        Solution ob;
+        cout<<ob.wordBreak(line, dict)<<"\n";
+    }
+}
+
+// } Driver Code Ends
